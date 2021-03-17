9ae1b32c7276fddb34594e5185e8e81e6478f988
Peter Schaldenbrand <pls21@pitt.edu>
2021-03-17 16:12:10 -0400

    Updating to most recent mockups

--- main.js
+++ main.js
@@ -5,27 +5,29 @@ function draw() {
     ctx.fillStyle = '#F4F6F8';
     ctx.beginPath();
     ctx.moveTo(0, 0);
-    
+
     // ctx.lineTo(0, 80);
     ctx.lineTo(1800, 500);
     ctx.lineTo(3000, 1200);
 
     ctx.lineTo(3000, 0);
-    
+
     ctx.fill();
     ctx.closePath();
   }
 }
 
 function initialize() {
-    draw();
+    try {
+        draw();
 
-    let returning = localStorage['visited'];
-    if (!returning) {
-        localStorage['visited'] = true;
-    }
+        let returning = localStorage['visited'];
+        if (!returning) {
+            localStorage['visited'] = true;
+        }
 
-    $('#covid-modal').modal({
-        show: !returning
-    });
+        $('#covid-modal').modal({
+            show: !returning
+        });
+    } catch(err) {}
 }
