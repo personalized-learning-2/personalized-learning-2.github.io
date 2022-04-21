5890a090f7b94daada0b0dddc07f296c2fef8246
jmkopko <jmkopko@gmail.com>
2022-04-21 12:47:54 -0400

    Release Notes 4.0
    Add 4.0 release notes; add navbar to release notes so that old versions can be viewed; release notes tests dynamic header/footer; add SMART pdf

--- main.js
+++ main.js
@@ -35,16 +35,73 @@ function initialize() {
 function pageNavSetup() {
   // this is for testing; gets used to dynamically load header and footer
 
-  $(document).ready(function() {
-    $("#header").load("header.html");
-    $("#footer").load("footer.html");
-  });
+  // $(document).ready(function() {
 
-  $(window).on("load", function() {
-    selectedPage = $("#page-title").text().toLowerCase();
-    $("#"+selectedPage+"-nav").addClass('active');
-    $(this).scrollTop(0);
-  });
+    $("#banner").load("banner.html", function() {
+      $("#header").load("header.html", function() {
+        $("#footer").load("footer.html", function() {
+          var selectedPage = $("#page-title").text().toLowerCase();
+          $("#"+selectedPage+"-nav").addClass('active');
+          document.getElementById("content").style.display = "block";
+        });
+      });
+    });
+
+  // }); 
+
+  // $(window).on("load", function() {
+
+  // });
+
+}
+
+function releaseNotesSetup() {
+  // this loads the most current release notes on get
+  // MUST BE UPDATED WITH NEW RELEASE NOTES
+    $("#release-notes_4.0").addClass("font-weight-bold");
+    //BUG ^ this bold isn't working and I have no idea why
+    // maybe something to do with it happening before the DOM is loaded?
+
+
+    $("#release-notes-body").load("release-notes_4.0.html");
+    console.log($("#release-notes_4.0"));
+
+}
+
+function releaseNav(item) {
+
+  // when adding new release notes, make sure the filename follows convention, and the new
+  // <li> in release_notes.html has the same id as the filename!
+
+  // have to hide and re-show footer to avoid visual glitch
+  // document.getElementById("footer").style.display = "none";
+
+
+  $(".release_nav").removeClass("font-weight-bold");
+  $(item).addClass("font-weight-bold");
+  
+
+  $("#footer").addClass("d-none");
+    $("#release-notes-body").empty();
+
+  
+  
+    $("#release-notes-body").load(item.id+".html", function() {
+      $("#footer").removeClass("d-none");
+    });
+  
+  // $("#release-notes-body").empty();
+
+  // $(".release-nav").removeClass("font-weight-600");
+  // $("#item.id").addClass("font-weight-600");
+
+
+  // $("#release-notes-body").load(item.id+".html", function() {
+
+  // });
+
+  // document.getElementById("footer").style.display = "block";
+
 }
 
 function contactUs() {
@@ -71,3 +128,38 @@ function contactUs() {
 
     return false;
 }
+
+// var $form = $('form#contact-us-form');
+
+function sendContactForm() {
+  
+  user_data = {
+    "Name": document.getElementById("contact-form-name").value,
+    "Email": document.getElementById("contact-form-email").value,
+    "Institution": document.getElementById("contact-form-inst").value,
+    "MoreInfo": document.getElementById("contact-form-moreinfo").value,
+    "Services":"pl2 Toolkit, pl2 Tutoring"
+  }
+  
+  $.ajax({
+        url: "https://script.google.com/macros/s/AKfycby6e6dvopVzMSGZfMesDU_m1Msz8fHlv9A3yifb2v4H7tpADEQIm5JhvBbRZmQZQ-dn/exec",
+        method: "GET",
+        dataType: "json",
+        data: user_data
+      })
+  
+
+}
+
+// $('#contact-us-form').on('click', function(e) {
+//   e.preventDefault();
+//   var jqxhr = $.ajax({
+//     url: "https://script.google.com/macros/s/AKfycbzS-RLdfTGccsy1d2_0w7eEGA7UgRF_-uq_BWUPiQGWrg09WxzC4fZ6Rt7o30b5jWVV/exec",
+//     method: "GET",
+//     dataType: "json",
+//     data: $form.serializeObject()
+//   }).success(
+//     // do something
+//     function() {console.log("Successful post!");}
+//   );
+// })
